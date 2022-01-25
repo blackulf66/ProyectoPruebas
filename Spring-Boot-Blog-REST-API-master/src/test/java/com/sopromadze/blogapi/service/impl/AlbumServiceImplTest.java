27f966b56d22306a96f3fdf5cdc52ebe2e232140
javi navarro <tu@ejemplo.comlobosaur22@gmail.com>
2022-01-25 23:18:53 +0100

    intento de prueba numero 2

--- Spring-Boot-Blog-REST-API-master/src/test/java/com/sopromadze/blogapi/service/impl/AlbumServiceImplTest.java
+++ Spring-Boot-Blog-REST-API-master/src/test/java/com/sopromadze/blogapi/service/impl/AlbumServiceImplTest.java
@@ -2,12 +2,14 @@ package com.sopromadze.blogapi.service.impl;
 
 import com.sopromadze.blogapi.model.Album;
 import com.sopromadze.blogapi.model.user.User;
+import com.sopromadze.blogapi.payload.AlbumResponse;
 import com.sopromadze.blogapi.payload.PagedResponse;
 import com.sopromadze.blogapi.repository.AlbumRepository;
 import com.sopromadze.blogapi.repository.UserRepository;
 import com.sopromadze.blogapi.service.AlbumService;
 import com.sopromadze.blogapi.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -18,7 +20,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -29,40 +33,62 @@ import static org.mockito.Mockito.when;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class AlbumServiceImplTest {
 
-    @Autowired
+
     private AlbumService albumService;
 
+    private Album album;
+    private AlbumResponse albumResponse;
+    
+    private final ModelMapper modelMapper;
     @Autowired
     private AlbumRepository albumRepository;
 
-    @Autowired
+
     private UserService userService;
 
-    @Autowired
+
     private UserRepository userRepository;
+
+    AlbumServiceImplTest() {
+        modelMapper = null;
+    }
+
     @Test
     void test_getAllAlbumsService() {
 
-        Album album = new Album();
-        album.setTitle("El album");
+        album = new Album();
+        album.setTitle("dasdsad");
+        album.setCreatedAt(Instant.now());
+        album.setUpdatedAt(Instant.now());
+        albumResponse = new AlbumResponse();
+        albumResponse.setTitle("fdgfdgfdg");
+        album.setCreatedAt(Instant.now());
+        album.setUpdatedAt(Instant.now());
 
-        Page<Album> pageResult = new PageImpl<>(Arrays.asList(album));
-        PagedResponse<Album> result = new PagedResponse<>();
+        List<AlbumResponse> data2 = Arrays.asList(albumResponse);
+        Page<Album> data = new PageImpl<>(Arrays.asList(album));
 
-        result.setContent(pageResult.getContent());
+        PagedResponse<AlbumResponse> result = new PagedResponse<>();
+
+        result.setContent(data2);
         result.setTotalPages(1);
         result.setTotalElements(1);
         result.setLast(true);
         result.setSize(1);
 
-        when(albumRepository.findByCreatedBy(any(Long.class), any(Pageable.class))).thenReturn(pageResult);
+        List<Album> albums = new ArrayList<>();
+        albums.add(album);
+        AlbumResponse [] albumResponses = {albumResponse};
 
-        assertEquals(result, albumService.getAllAlbums(0 , 1));
+        when(albumRepository.findAll(any(Pageable.class))).thenReturn(data);
+        when(modelMapper.map(data.getContent(), AlbumResponse[].class)).thenReturn(albumResponses);
+
+        assertEquals(result, albumService.getAllAlbums(1,1));
 
     }
 
-    @Test
-    void test_addAllAlbumService() {
+   /* @Test
+    void test_addAllAlbumService1() {
 
         User user = new User();
         user.setUsername("user");
@@ -85,5 +111,5 @@ class AlbumServiceImplTest {
 
         assertEquals(result, albumService.addAlbum(albumRequest, user));
 
-    }
+    }*/
 }
