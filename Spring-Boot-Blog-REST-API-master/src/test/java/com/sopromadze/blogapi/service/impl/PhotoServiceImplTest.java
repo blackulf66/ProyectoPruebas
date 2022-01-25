7e748e39b6025d1136617585fc1c3c893beb1fab
Alfonso Gallardo Rodríguez <alfonso@MBP-de-Alfonso.home>
2022-01-25 23:57:33 +0100

    Añado método findAll, pero no termina de funcionar

--- Spring-Boot-Blog-REST-API-master/src/test/java/com/sopromadze/blogapi/service/impl/PhotoServiceImplTest.java
+++ Spring-Boot-Blog-REST-API-master/src/test/java/com/sopromadze/blogapi/service/impl/PhotoServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.sopromadze.blogapi.service.impl;
 
 import com.sopromadze.blogapi.model.Photo;
+import com.sopromadze.blogapi.payload.PagedResponse;
 import com.sopromadze.blogapi.repository.PhotoRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,9 +10,16 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.parameters.P;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
 public class PhotoServiceImplTest {
@@ -27,6 +35,18 @@ public class PhotoServiceImplTest {
 
         Photo photo = new Photo();
         photo.setTitle("Foto");
+
+        Page<Photo> pageResult = new PageImpl<>(Arrays.asList(photo));
+
+        PagedResponse<Photo> result = new PagedResponse<>();
+        result.setContent(pageResult.getContent());
+        result.setTotalPages(1);
+        result.setTotalElements(1);
+        result.setLast(true);
+        result.setSize(1);
+
+
+        assertEquals(result, photoService.getAllPhotos(0,10));
 
     }
 
