ae720849df2ca215d18f95e71b6ce7e36953cbbc
Daniel De Luna Rodríguez <deluna.rodan21@triana.salesianos.edu>
2022-01-26 13:21:21 +0100

    Continuo con el testeo del servicio de categoría y arreglo devolución de métodos

--- Spring-Boot-Blog-REST-API-master/src/main/java/com/sopromadze/blogapi/service/CategoryService.java
+++ Spring-Boot-Blog-REST-API-master/src/main/java/com/sopromadze/blogapi/service/CategoryService.java
@@ -11,13 +11,13 @@ public interface CategoryService {
 
 	PagedResponse<Category> getAllCategories(int page, int size);
 
-	ResponseEntity<Category> getCategory(Long id);
+	Category getCategory(Long id);
 
-	ResponseEntity<Category> addCategory(Category category, UserPrincipal currentUser);
+	Category addCategory(Category category, UserPrincipal currentUser);
 
-	ResponseEntity<Category> updateCategory(Long id, Category newCategory, UserPrincipal currentUser)
+	Category updateCategory(Long id, Category newCategory, UserPrincipal currentUser)
 			throws UnauthorizedException;
 
-	ResponseEntity<ApiResponse> deleteCategory(Long id, UserPrincipal currentUser) throws UnauthorizedException;
+	void deleteCategory(Long id, UserPrincipal currentUser) throws UnauthorizedException;
 
 }
--- Spring-Boot-Blog-REST-API-master/src/test/java/com/sopromadze/blogapi/service/CategoryServiceImplTest.java
+++ Spring-Boot-Blog-REST-API-master/src/test/java/com/sopromadze/blogapi/service/CategoryServiceImplTest.java
@@ -3,11 +3,12 @@ package com.sopromadze.blogapi.service;
 import com.sopromadze.blogapi.model.Category;
 import com.sopromadze.blogapi.payload.PagedResponse;
 import com.sopromadze.blogapi.repository.CategoryRepository;
+import com.sopromadze.blogapi.security.UserPrincipal;
 import com.sopromadze.blogapi.service.impl.CategoryServiceImpl;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -18,7 +19,8 @@ import java.util.Arrays;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+
 import org.springframework.data.domain.Page;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -29,6 +31,7 @@ public class CategoryServiceImplTest {
 
     @Mock
     private final ModelMapper modelMapper;
+
     CategoryServiceImplTest() {
         modelMapper = null;
     }
@@ -41,7 +44,7 @@ public class CategoryServiceImplTest {
 
     //TODO: Daniel de Luna Rodríguez
     @Test
-    void test_getAllCategories(){
+    void test_getAllCategories() {
 
         Category cat1 = new Category("dummy_category");
         Category cat2 = new Category("dummy_category2");
@@ -58,28 +61,55 @@ public class CategoryServiceImplTest {
 
         List<Category> categories = new ArrayList<>();
 
-        Category [] categoryResponses = {cat2};
+        Category[] categoryResponses = {cat2};
         categories.add(cat1);
 
         when(categoryRepository.findAll(any(Pageable.class))).thenReturn(pageResult);
         when(modelMapper.map(pageResult.getContent(), Category[].class)).thenReturn(categoryResponses);
 
-        assertEquals(result, categoryService.getAllCategories(1,1));
+        assertEquals(result, categoryService.getAllCategories(1, 1));
     }
 
     @Test
-    void test_getCategorySuccess(){
+    void test_getCategorySuccess() {
+        Category cat1 = new Category("dummy_category");
+        cat1.setId(1L);
+        categoryRepository.save(cat1);
+        when(categoryRepository.getById(1L)).thenReturn(cat1);
+        assertEquals(cat1, categoryRepository.getById(1L));
+    }
+
+    @Test
+    void test_addCategorySuccess(){
+        Category cat1 = new Category("dummy_category");
+        cat1.setId(1L);
+        UserPrincipal user_prueba = mock(UserPrincipal.class);
+        when(categoryService.addCategory(cat1,user_prueba)).thenReturn(cat1);
+        assertEquals(categoryService.addCategory(cat1,user_prueba),cat1);
+    }
+    /*
+    @Test
+    void test_updateCategorySuccess() {
+        Category cat1 = new Category("dummy_category");
+        cat1.setId(1L);
+        categoryRepository.save(cat1);
+        UserPrincipal user_prueba = Mockito.mock(UserPrincipal.class);
+
+        when
+    }
+    */
+
+    @Test
+    void test_deletedCategorySuccess(){
+
+        CategoryServiceImpl catService = mock(CategoryServiceImpl.class);
+        UserPrincipal user_prueba = mock(UserPrincipal.class);
 
         Category cat1 = new Category("dummy_category");
-
         cat1.setId(1L);
-
         categoryRepository.save(cat1);
 
-        when(categoryRepository.getById(1L)).thenReturn(cat1);
-
-        assertEquals(cat1, categoryRepository.getById(1L));
-
+        doNothing().when();
 
     }
 }
