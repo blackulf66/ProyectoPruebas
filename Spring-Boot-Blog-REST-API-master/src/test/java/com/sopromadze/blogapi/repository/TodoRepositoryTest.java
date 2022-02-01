c19e38c78ccff264aa1f18f24d1c0e5bb07938d0
Daniel <danidlr02@gmail.com>
2022-02-01 18:50:37 +0100

    Tests de comment y todo repos actualizados

--- Spring-Boot-Blog-REST-API-master/src/test/java/com/sopromadze/blogapi/repository/TodoRepositoryTest.java
+++ Spring-Boot-Blog-REST-API-master/src/test/java/com/sopromadze/blogapi/repository/TodoRepositoryTest.java
@@ -1,14 +1,27 @@
 package com.sopromadze.blogapi.repository;
 
 
+import com.sopromadze.blogapi.model.Comment;
+import com.sopromadze.blogapi.model.Todo;
+import com.sopromadze.blogapi.model.user.User;
+import org.hibernate.mapping.Any;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -27,6 +40,25 @@ public class TodoRepositoryTest {
     @Test
     void test_findByCreatedBySuccess(){
 
+        User user_prueba = new User("user","de prueba","username12","emailtest@gmail.com","qwerty");
+        user_prueba.setCreatedAt(Instant.now());
+        user_prueba.setUpdatedAt(Instant.now());
+
+        testEntityManager.persist(user_prueba);
+
+        Todo todo = new Todo();
+        todo.setTitle("Título todo");
+        todo.setUser(user_prueba);
+        todo.setCreatedBy(user_prueba.getId());
+        todo.setUpdatedBy(user_prueba.getId());
+        todo.setUpdatedAt(Instant.now());
+        todo.setCreatedAt(Instant.now());
+
+        testEntityManager.persist(todo);
+
+        assertEquals(List.of(todo), todoRepository.findByCreatedBy(user_prueba.getId(), any(PageRequest.class)).getContent());
+
     }
+    
 
 }
