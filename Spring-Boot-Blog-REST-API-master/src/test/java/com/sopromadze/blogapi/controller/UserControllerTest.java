38de4c351626d354e919da0e8d01eac8f24aa43c
Daniel De Luna Rodríguez <deluna.rodan21@triana.salesianos.edu>
2022-02-02 12:07:13 +0100

    Empiezo el controller de user

--- Spring-Boot-Blog-REST-API-master/src/test/java/com/sopromadze/blogapi/controller/UserControllerTest.java
+++ Spring-Boot-Blog-REST-API-master/src/test/java/com/sopromadze/blogapi/controller/UserControllerTest.java
@@ -5,6 +5,8 @@ import com.sopromadze.blogapi.config.SpringSecurityTestWebConfig;
 import com.sopromadze.blogapi.model.role.Role;
 import com.sopromadze.blogapi.model.role.RoleName;
 import com.sopromadze.blogapi.model.user.User;
+import com.sopromadze.blogapi.payload.UserIdentityAvailability;
+import com.sopromadze.blogapi.payload.UserProfile;
 import com.sopromadze.blogapi.payload.UserSummary;
 import com.sopromadze.blogapi.security.UserPrincipal;
 import com.sopromadze.blogapi.service.CommentService;
@@ -23,11 +25,13 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
 import static org.modelmapper.internal.bytebuddy.matcher.ElementMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -57,16 +61,75 @@ public class UserControllerTest {
 
         usuario_test = new User("usuario","de prueba","username","testemail@gmail.com","qwerty");
         usuario_test.setId(1L);
+        usuario_test.setRoles(List.of(new Role(RoleName.ROLE_ADMIN)));
     }
 
-   /* @Test
+    @Test
     @DisplayName("GET /api/users/me")
     @WithMockUser(username = "userMock", password = "pwd", roles = "USER")
-    void test_getCurrentUser() throws Exception {
+    void test_getCurrentUserSueccess() throws Exception {
+
+
+        UserPrincipal userPrincipal = UserPrincipal.create(usuario_test);
+
+        UserSummary result = new UserSummary(usuario_test.getId(), usuario_test.getUsername(),
+                usuario_test.getFirstName(),usuario_test.getLastName());
+
+        when(userService.getCurrentUser(userPrincipal)).thenReturn(result);
+
         mockMvc.perform(get("/api/users/me")
                         .contentType("application/json"))
-                .andExpect(content().json(objectMapper.writeValueAsString(UserSummary.class)))
                 .andExpect(status().isOk()).andDo(print());
 
-    }*/
+    }
+
+    @Test
+    @DisplayName("GET /api/users/checkUsernameAvailability")
+    void test_checkUsernameAvailabilitySuccess() throws Exception{
+
+        UserIdentityAvailability noDisponible = new UserIdentityAvailability(false);
+
+        when(userService.checkUsernameAvailability(usuario_test.getUsername())).thenReturn(noDisponible);
+
+        mockMvc.perform(get("/api/users/checkUsernameAvailability")
+                        .param("username", usuario_test.getUsername())
+                        .contentType("application/json"))
+                .andExpect(content().json(objectMapper.writeValueAsString(noDisponible)))
+                .andExpect(status().isOk()).andDo(print());
+
+    }
+
+    @Test
+    @DisplayName("GET /api/users/checkEmailAvailability")
+    void test_checkEmailAvailabilitySuccess() throws Exception{
+
+        UserIdentityAvailability noDisponible = new UserIdentityAvailability(false);
+
+        when(userService.checkEmailAvailability(usuario_test.getEmail())).thenReturn(noDisponible);
+
+        mockMvc.perform(get("/api/users/checkEmailAvailability")
+                        .param("email", usuario_test.getEmail())
+                        .contentType("application/json"))
+                .andExpect(content().json(objectMapper.writeValueAsString(noDisponible)))
+                .andExpect(status().isOk()).andDo(print());
+
+    }
+
+    @Test
+    @DisplayName("GET /api/users/{username}/profile")
+    void test_getUSerProfileSuccess() throws Exception {
+
+        UserProfile userProfile = new UserProfile(usuario_test.getId(),usuario_test.getUsername(),
+                usuario_test.getFirstName(),usuario_test.getLastName(), Instant.now(),
+                usuario_test.getEmail(),usuario_test.getAddress(), usuario_test.getPhone(),
+                usuario_test.getWebsite(),usuario_test.getCompany(),
+                0L);
+
+        when(userService.getUserProfile(usuario_test.getUsername())).thenReturn(userProfile);
+        mockMvc.perform(get("/api/users/{username}/profile",usuario_test.getUsername())
+                        .contentType("application/json"))
+                .andExpect(content().json(objectMapper.writeValueAsString(userProfile)))
+                .andExpect(status().isOk()).andDo(print());
+    }
+
 }
